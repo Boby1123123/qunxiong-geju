2bbd60f6b9f1437c406e13c14293b9269bfad8b7
Boby1123123 <326953867+Boby1123123@users.noreply.github.com>
2026-09-11 15:18:03 +0800

    C-2 脚本归档

--- tools/scripts_tmp/c1_list_nodes.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import io, re
-for f in [r"dn_finale.js", r"dn_warphase.js"]:
-    p = r"D:\1pao tuan\群雄割据\src\data_nodes\\" + f
-    t = io.open(p, encoding='utf-8').read()
-    ids = re.findall(r'N\["([^"]+)"\]', t)
-    print(f, 'nodes:', len(ids))
-    print(ids)
